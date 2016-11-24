1a2ec02705150fb4d16dee67efb17f7356b0e99c
abachar <a.bachar@hotmail.fr>
2016-11-24 22:33:19 +0100

    Add unit tests

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var package = require('./package.json');
 var path = require('path');
 
-var platform = process.platform
+function getDriverName() {
+  var platform = process.platform;
 
-if (platform === 'linux') {
-    platform += (process.arch === 'x64') ? '64' : '32'
-} else if (platform === 'darwin') {
-  if (process.arch === 'x64') {
-    platform = 'mac64'
+  if (platform === 'linux') {
+      platform += (process.arch === 'x64') ? '64' : '32'
+  } else if ((platform === 'darwin') && (process.arch === 'x64')) {
+      platform = 'mac64'
+  } else if (platform === 'win32') {
+    platform += '.exe'
   } else {
-    console.log('Only Mac 64 bits supported.');
-    process.exit(1);
+    console.log('Unexpected platform or architecture:', process.platform, process.arch)
+    process.exit(1)
   }
-} else if (platform === 'win32') {
-  platform += '.exe'
-} else {
-  console.log('Unexpected platform or architecture:', process.platform, process.arch)
-  process.exit(1)
+
+  return 'chromedriver_' + platform;
 }
 
 module.exports = {
-  path: path.resolve(__dirname, 'binaries/chromedriver_' + platform),
+  getDriverName: getDriverName,
+  path: path.resolve(__dirname, 'binaries', getDriverName()),
   version: package.version
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -5,26 +5,62 @@ var fs = require('fs');
 var driver = require('../index');
 
 describe('chrome-driver-standalone', function() {
-  describe('path', function() {
-    it('should define path', function() {
-      expect(driver.path).to.exist;
-    });
-
-    //it('should be an absolute path', function() {
-      // expect(path.resolve(subject.path)).to.equal(subject.path);
-    //});
-
-    //it('should exist', function(done) {
-      // fs.exists(subject.path, function(exists) {
-      //  expect(exists).to.be.true;
-      //  done();
-      //});
-    // });
-  });
+  var original_platform;
+  var original_arch;
 
   describe('version', function() {
     it('should be a valid version', function() {
-      expect(driver.version).to.match(/^\d+\.\d+\.\d+$/)
+      expect(driver.version).to.match(/^\d+\.\d+\.\d+$/);
+    });
+  });
+
+  describe('path', function() {
+    it('should be an absolute path', function() {
+      expect(path.resolve(driver.path)).to.equal(driver.path);
+    });
+
+    it('should exist', function(done) {
+      fs.exists(driver.path, function(exists) {
+        expect(exists).to.be.true;
+        done();
+      });
+    });
+  });
+
+  describe('driver name', function() {
+    function given_platfor(platform, arch) {
+      Object.defineProperty(process, 'platform', { value: platform });
+      Object.defineProperty(process, 'arch', { value: arch });
+    }
+
+    beforeEach(function() {
+      original_platform = Object.getOwnPropertyDescriptor(process, 'platform');
+      original_arch = Object.getOwnPropertyDescriptor(process, 'arch');
+    });
+
+    afterEach(function() {
+      Object.defineProperty(process, 'platform', original_platform);
+      Object.defineProperty(process, 'arch', original_arch);
+    });
+
+    it('should use linux x32 driver ', function() {
+      given_platfor('linux');
+      expect(driver.getDriverName()).to.equal('chromedriver_linux32');
+    });
+
+    it('should use linux x64 driver ', function() {
+      given_platfor('linux', 'x64');
+      expect(driver.getDriverName()).to.equal('chromedriver_linux64');
+    });
+
+    it('should use osx driver ', function() {
+      given_platfor('darwin', 'x64');
+      expect(driver.getDriverName()).to.equal('chromedriver_mac64');
+    });
+
+    it('should use win32 driver ', function() {
+      given_platfor('win32');
+      expect(driver.getDriverName()).to.equal('chromedriver_win32.exe');
     });
   });
 });
